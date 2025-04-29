8217ff4c1836a6a728ef1ca56c34f0950c42a5cf
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-29 19:58:34 +0200

    feat: enhance rounding logic in roundToHalf function for improved accuracy

--- src/main/kotlin/dev/slne/surf/utils/star-utils.kt
+++ src/main/kotlin/dev/slne/surf/utils/star-utils.kt
@@ -1,5 +1,9 @@
 package dev.slne.surf.utils
 
+import java.math.BigDecimal
+import java.math.RoundingMode
+import kotlin.math.floor
+import kotlin.math.round
 import kotlin.math.roundToInt
 
 fun buildStarString(glyphs: Glyphs = Glyphs.STAR_NO_BG, value: Double, maxValue: Double = 5.0): String {
@@ -44,5 +48,13 @@ private fun appendSeparator(builder: StringBuilder, glyphs: Glyphs) {
 }
 
 fun Double.roundToHalf(): Double {
-    return if (this % 1 < 0.5) this.toInt().toDouble() else this.toInt() + 0.5
+    println("Rounding $this to half")
+
+    val rounded = BigDecimal(this).setScale(1, RoundingMode.HALF_UP).toDouble()
+    val base = floor(rounded)
+    val decimal = rounded - base
+
+    println("Rounded $this to $rounded")
+
+    return if (decimal < 0.5) base else base + 0.5
 }
