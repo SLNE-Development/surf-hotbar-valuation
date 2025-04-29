331a5d1ddbd1e3080f43ae57caa3a0e8836e53cc
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-04-29 17:31:06 +0200

    feat: implement current plot rating value placeholder logic

--- src/main/kotlin/dev/slne/surf/expansion/placeholder/SurfCurrentPlotRatingValuePlaceholder.kt
+++ src/main/kotlin/dev/slne/surf/expansion/placeholder/SurfCurrentPlotRatingValuePlaceholder.kt
@@ -1,13 +1,25 @@
 package dev.slne.surf.expansion.placeholder
 
+import dev.slne.surf.PossibleValuations
+import dev.slne.surf.p2.P2Manager
+import dev.slne.surf.selectedValuation
 import dev.slne.surf.surfapi.bukkit.api.hook.papi.expansion.PapiPlaceholder
+
 import org.bukkit.OfflinePlayer
+import org.bukkit.entity.Player
 
 class SurfCurrentPlotRatingValuePlaceholder(): PapiPlaceholder("current_rating_value") {
     override fun parse (
         player: OfflinePlayer,
         args: List<String>
     ): String {
-        TODO("Not yet implemented")
+        return when(selectedValuation) {
+            PossibleValuations.PLOTSQUARED -> {
+                val user = player as? Player ?: return ""
+                val plot = P2Manager.getStandingPlot(user) ?: return ""
+
+                (plot.averageRating * 2).toString()
+            }
+        }
     }
 }
